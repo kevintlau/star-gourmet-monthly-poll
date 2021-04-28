32f1bb52e98d577d2cf2eb6ca5102d4a8b9aa4b0
Kevin Lau <lau.kevt@gmail.com>
2021-04-28 00:03:19 -0700

    finishes api and cleans up pagination

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@
 // ------- constant variables -------------------------------------------------
 
 // magic variables - choose how many results and page buttons to show per page
-let RESULTS_PER_PAGE = 12;
+let RESULTS_PER_PAGE = 6;
 let NUM_OF_PAGE_BTNS = 5;
 
 // ------- state variables ----------------------------------------------------
@@ -30,17 +30,15 @@ $voteBtnEl.on("click", handleVote);
 
 // ------- functions ----------------------------------------------------------
 
+init();
+
 // init - get the recipes list from server and render onto page
 function init() {
-  loadRecipes();
-  paginate();
-  render();
-}
-
-// get results
-function loadRecipes() {
   $.get("/api/recipes").then(
-    (data) => (fullResults = data),
+    (data) => {
+      fullResults = data;
+      render();
+    },
     (err) => console.log(err)
   );
 }
@@ -59,30 +57,31 @@ function paginate() {
 function generatePageButtons() {
   $pageBtnWrapperEl.empty();
   let pageButtonWindow = calculateButtonWindow();
-  let btnsHtml;
+  console.log(pageButtonWindow);
+  let btnsHtml = "";
   // create a "First" button as the first button if the current page is not 1
   if (currentPage != 1) {
-    btnsHtml.concat(`
-      <button value="1" class="page btn btn-sm btn-info">
+    btnsHtml += `
+      <button value=${1} class="page-btn btn btn-sm btn-info">
         &#171; First
       </button>
-    `);
+    `;
   };
   // fill the numerical buttons around the current page
   for (let i = pageButtonWindow[0]; i <= pageButtonWindow[1]; i++) {
-    btnsHtml.concat(`
+    btnsHtml += `
       <button value=${i} class="page-btn btn btn-sm btn-info">
         ${i}
       </button>
-    `);
+    `;
   };
   // create a "Last" button as the last button if the current page is not last
   if (currentPage != numOfPages) {
-    btnsHtml.concat(`
-      <button value="${numOfPages}" class="page btn btn-sm btn-info">
+    btnsHtml += `
+      <button value=${numOfPages} class="page-btn btn btn-sm btn-info">
         Last &#187;
       </button>
-    `);
+    `;
   };
   $pageBtnWrapperEl.append(btnsHtml);
 }
@@ -103,17 +102,21 @@ function calculateButtonWindow() {
     // special case: if window is too big, then hardcode 1 as maxLeft
     if (maxLeft < 1) maxLeft = 1;
   }
+  console.log("calculateButtonWindow result: ", [maxLeft, maxRight]);
   return [maxLeft, maxRight];
 }
 
 function render() {
-    let resultsHtml = pagedResults.map((result) => {
-        return `<article class="recipe">
+  paginate();
+  let resultsHtml = pagedResults.map((recipe) => {
+    return `
+      <article class="recipe">
         <h3>${recipe.name}</h3>
-        </article>`;
-    });
-    $recipesWrapperEl.append(resultsHtml);
-    generatePageButtons();
+      </article>
+    `;
+  });
+  $recipesWrapperEl.append(resultsHtml);
+  generatePageButtons();
 }
 
 function handleChangePage() {
